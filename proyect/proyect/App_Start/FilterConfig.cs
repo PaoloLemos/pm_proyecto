d79c101edadf74f6096c96571ab329dca46dd855
PaoloLemos <lemospaolo2@gmail.com>
2025-06-05 15:55:32 -0300

    Primer commit del proyecto

---
+++ proyect/proyect/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace proyect
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
