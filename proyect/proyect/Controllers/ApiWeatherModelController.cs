9fe9d303760a2659b049fd240e72434ac2ec7fca
PaoloLemos <156336242+PaoloLemos@users.noreply.github.com>
2025-06-17 22:09:25 -0300

    commit paolo

---
+++ proyect/proyect/Controllers/ApiWeatherModelController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using proyect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyect.Controllers
+{
+    public class ApiWeatherModelController : Controller
+    {
+        private readonly string apiKey = "352197478cb79204e7cffd1537f3f77c";
+
+        public async Task<ActionResult> clima()
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat=-34.9&lon=-54.95&appid={apiKey}&units=metric&lang=es";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetStringAsync(url);
+                    var clima = JsonConvert.DeserializeObject<ApiWeatherModel>(response);
+                    return View(clima);
+                }
+                catch (HttpRequestException ex)
+                {
+                    ViewBag.Error = "No se pudo obtener el clima. Detalles: " + ex.Message;
+                    return View();
+                }
+            }
+        }
+    }
+}
